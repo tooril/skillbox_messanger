fcee149ede24f531755c15f7cfc5b11d938c5546
Alexey Tokarev <admin@ghost17.ru>
2022-04-19 19:27:30 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import datetime
+
+
+def print_message(mess):
+    print(f"[{mess['sender']}]: {mess['text']} / {mess['time']}")
+
+def add_message(sender, text):
+    # 1. Подготовить словарь с данными сообщения
+    new_message = {
+        "sender": sender,
+        "text": text,
+        "time": "19:22"  # ToDO: Здесь должно быть текущее время
+    }
+    all_messages.append(new_message)
+
+message1 = {
+    'sender': "Яг Янов",
+    'text': "А конфеты вы тоже за меня будете есть?",
+    'time': '22:55'
+}
+message2 = {
+    'sender': "Еще кто-то?",
+    'text': "Что делать?",
+    'time': '22:55'
+}
+
+all_messages = [message1, message2]
+
+add_message("Митя", "Всем привет")
+
+for message in all_messages:
+    print_message(message)
